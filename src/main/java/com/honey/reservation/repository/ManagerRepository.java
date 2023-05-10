268527ec5f4bae80881fabd67a1cf8e9019e005c
hingjae <ljh23270@naver.com>
2023-05-10 12:26:00 +0900

    #8-repository 작성

---
+++ src/main/java/com/honey/reservation/repository/ManagerRepository.java
@@ -0,0 +1,7 @@
+package com.honey.reservation.repository;
+
+import com.honey.reservation.domain.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
+}
