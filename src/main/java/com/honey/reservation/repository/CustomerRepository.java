dd0b0817bf7ad22400b5dcaefc902344e0564048
hingjae <ljh23270@naver.com>
2023-06-09 16:42:11 +0900

    #19- 로그인 아이디 검색
    DB에 로그인 아이디가 존재하는지 검색.

--- src/main/java/com/honey/reservation/repository/CustomerRepository.java
+++ src/main/java/com/honey/reservation/repository/CustomerRepository.java
@@ -2,6 +2,12 @@ package com.honey.reservation.repository;
 
 import com.honey.reservation.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query("select c.loginId from Customer c where c.loginId = :loginId")
+    Optional<String> findLoginId(@Param("loginId") String loginId);
 }
--- src/test/java/com/honey/reservation/repository/CustomerRepositoryTest.java
+++ src/test/java/com/honey/reservation/repository/CustomerRepositoryTest.java
@@ -1,16 +1,24 @@
 package com.honey.reservation.repository;
 
 import com.honey.reservation.domain.Customer;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.Optional;
 
-@Transactional
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("CustomerRepository 테스트")
+@Import(CustomerRepositoryTest.TestJpaConfig.class)
+@DataJpaTest
 class CustomerRepositoryTest {
 
     @Autowired CustomerRepository customerRepository;
@@ -27,6 +35,32 @@ class CustomerRepositoryTest {
         customerRepository.flush();
 
         customerRepository.delete(customer);
+    }
 
+    @DisplayName("로그인 아이디 DB에 존재 O")
+    @Test
+    void givenExistingLoginId_whenSelectLoginId_thenResultIsNotEmpty() {
+        customerRepository.save(Customer.of(null, "loginId", "password", "name", "phoneNumber"));
+        customerRepository.flush();
+        Optional<String> findLoginId = customerRepository.findLoginId("loginId");
+        assertThat(findLoginId).isNotEmpty();
+    }
+
+    @DisplayName("로그인 아이디 DB에 존재 X")
+    @Test
+    void givenNotExistingLoginId_whenSelectLoginId_thenResultIsEmpty() {
+        customerRepository.save(Customer.of(null, "loginId", "password", "name", "phoneNumber"));
+        customerRepository.flush();
+        Optional<String> findLoginId = customerRepository.findLoginId("honey");
+        assertThat(findLoginId).isEmpty();
+    }
+
+    @EnableJpaAuditing
+    @TestConfiguration
+    static class TestJpaConfig {
+        @Bean
+        AuditorAware<String> auditorAware() {
+            return () -> Optional.of("uno");
+        }
     }
 }
