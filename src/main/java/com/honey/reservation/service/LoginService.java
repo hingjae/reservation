d6a873005e11c9dc8be33792bfe871a13fe9df4a
hingjae <ljh23270@naver.com>
2023-06-09 21:54:22 +0900

    #23 - loadByUsername
    시큐리티 UserDetailsService 메서드 loadByUsername을 구현함

--- src/main/java/com/honey/reservation/service/LoginService.java
+++ src/main/java/com/honey/reservation/service/LoginService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -15,8 +17,13 @@ public class LoginService {
 
     public void signUpCustomer(CustomerDto dto) {
         validateDuplicateLoginId(dto.loginId());
-
         customerRepository.save(dto.toEntity());
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<CustomerDto> loadUserByUsername(String username) {
+        return customerRepository.findByLoginId(username)
+                .map(CustomerDto::from);
     }
 
     private void validateDuplicateLoginId(String loginId) {
