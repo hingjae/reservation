f75fd1f8fbc31f02bf4532da4de868db11b56643
hingjae <ljh23270@naver.com>
2023-06-09 16:38:12 +0900

    #19-CustomerDto 변경
    dto에 필드 변수 loginId를 추가하고 dto스펙에 맞게 테스트도 수정해줌.

--- src/main/java/com/honey/reservation/dto/CustomerDto.java
+++ src/main/java/com/honey/reservation/dto/CustomerDto.java
@@ -3,15 +3,17 @@ package com.honey.reservation.dto;
 import com.honey.reservation.domain.Customer;
 
 public record CustomerDto(
-        Long id, String name, String phoneNumber
+        Long id, String loginId, String password, String name, String phoneNumber
 ) {
-    public static CustomerDto of(Long id, String name, String phoneNumber) {
-        return new CustomerDto(id, name, phoneNumber);
+    public static CustomerDto of(Long id, String loginId, String password, String name, String phoneNumber) {
+        return new CustomerDto(id, loginId, password, name, phoneNumber);
     }
 
     public static CustomerDto from(Customer customer) {
-        return new CustomerDto(
-                customer.getId(), customer.getName(), customer.getPhoneNumber()
-        );
+        return CustomerDto.of(customer.getId(), customer.getLoginId(), customer.getPassword(), customer.getName(), customer.getPhoneNumber());
+    }
+
+    public Customer toEntity() {
+        return Customer.of(loginId, password, name, phoneNumber);
     }
 }
