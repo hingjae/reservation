b34aea4434e831079b7083c7470ba69ee59579d6
hingjae <ljh23270@naver.com>
2023-06-13 23:40:43 +0900

    #33-availableDateTime 메서드
    년, 월, 일을 쿼리 파라미터로 입력했을 때, 가능한 예약 시간대를 출력한다.

---
+++ src/test/java/com/honey/reservation/service/TransactionalTest.java
@@ -0,0 +1,71 @@
+package com.honey.reservation.service;
+
+import com.honey.reservation.domain.Customer;
+import com.honey.reservation.domain.Manager;
+import com.honey.reservation.domain.reservation.Reservation;
+import com.honey.reservation.domain.reservation.ReservationStatus;
+import com.honey.reservation.dto.YearDateDto;
+import com.honey.reservation.repository.CustomerRepository;
+import com.honey.reservation.repository.ManagerRepository;
+import com.honey.reservation.repository.ReservationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+public class TransactionalTest {
+
+    @Autowired ReservationService reservationService;
+    @Autowired ReservationRepository reservationRepository;
+    @Autowired CustomerRepository customerRepository;
+    @Autowired ManagerRepository managerRepository;
+
+    @Rollback(value = false)
+    @DisplayName("예약 가능 시간 검색")
+    @Test
+    void givenYearMonthDay_whenSearch_thenReturnAvailableTime() {
+        Customer customer = createCustomer();
+        Manager manager = createManager();
+
+        customerRepository.saveAndFlush(customer);
+        managerRepository.saveAndFlush(manager);
+
+
+        Reservation reservation1 = Reservation.of(customer, manager, 2023, 6, 13, 10.0, "memo", ReservationStatus.READY);
+        Reservation reservation2 = Reservation.of(customer, manager, 2023, 6, 13, 10.5, "memo", ReservationStatus.READY);
+        Reservation reservation3 = Reservation.of(customer, manager, 2023, 6, 13, 11.0, "memo", ReservationStatus.READY);
+        Reservation reservation4 = Reservation.of(customer, manager, 2023, 6, 13, 11.5, "memo", ReservationStatus.READY);
+
+
+        reservationRepository.save(reservation1);
+        reservationRepository.save(reservation2);
+        reservationRepository.save(reservation3);
+        reservationRepository.save(reservation4);
+        reservationRepository.flush();
+
+        YearDateDto dto = YearDateDto.of(2023, 6, 13);
+        List<Double> availableTimes = reservationService.availableDateTimeSearch(dto);
+        assertThat(availableTimes).isEqualTo(List.of(
+            9.0, 9.5, 12.0, 12.5, 13.0, 13.5, 14.0, 14.5, 15.0, 15.5, 16.0, 16.5, 17.0, 17.5
+        ));
+        List<String> collect = availableTimes.stream().map(String::valueOf).collect(Collectors.toList());
+        System.out.println("collect = " + collect);
+    }
+
+    private Manager createManager() {
+        return Manager.of("manager1", "pw1", "manager1");
+    }
+
+    private Customer createCustomer() {
+        return Customer.of("customer1", "pw1", "customer1", "1234");
+    }
+}
