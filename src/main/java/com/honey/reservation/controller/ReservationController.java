858f7ede1b6dd9f0adf072c32199643df1e61aaa
hingjae <ljh23270@naver.com>
2023-06-18 14:51:57 +0900

    예약 업데이트, 삭제 메서드 추가

--- src/main/java/com/honey/reservation/controller/ReservationController.java
+++ src/main/java/com/honey/reservation/controller/ReservationController.java
@@ -1,6 +1,7 @@
 package com.honey.reservation.controller;
 
 import com.honey.reservation.domain.reservation.ReservationStatus;
+import com.honey.reservation.dto.ReservationDto;
 import com.honey.reservation.dto.converter.LocalTimeConverter;
 import com.honey.reservation.dto.request.ReservationRequest;
 import com.honey.reservation.dto.response.ReservationTimeResponse;
@@ -11,12 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -51,7 +50,57 @@ public class ReservationController {
     ) {
         ReservationRequest reservationRequest = ReservationRequest.of(LocalDate.of(year, month, day), LocalTimeConverter.from(reservationTime), memo, ReservationStatus.READY);
         reservationService.save(reservationRequest.toDto(userAccountUserDetails.toDto()));
-        return "redirect:/";
+        return "redirect:/reservations/my-page";
     }
 
+    @GetMapping("/my-page")
+    public String myReservation(@AuthenticationPrincipal UserAccountUserDetails userAccountUserDetails, ModelMap map) {
+        List<ReservationDto> reservations = reservationService.findMyReservations(userAccountUserDetails);
+
+        map.addAttribute("name", userAccountUserDetails.name());
+
+        map.addAttribute("reservations", reservations);
+        return "reservations/my-page";
+    }
+
+    @GetMapping("{reservationId}/search-date")
+    public String updateReservationDatePage() {
+        return "reservations/update-reservation-calendar";
+    }
+
+    @GetMapping("{reservationId}/search-date/form")
+    public String updateReservationTimePage(
+            @RequestParam(name = "year", required = false) Integer year,
+            @RequestParam(name = "month", required = false) Integer month,
+            @RequestParam(name = "day", required = false) Integer day,
+            @PathVariable("reservationId") Long reservationId, ModelMap map
+    ) {
+        LocalDate reservationDate = LocalDate.of(year, month, day);
+        map.addAttribute("timeButtons", ReservationTimeResponse.from(reservationDate, reservationService.availableDateTimeSearch(reservationDate)).timeButtons());
+        map.addAttribute("date", reservationDate);
+        map.addAttribute("reservationId", reservationId);
+        return "reservations/update-reservation-form";
+    }
+
+    @PostMapping("{reservationId}/update")
+    public String updateReservation(
+            @PathVariable("reservationId") Long reservationId,
+            @AuthenticationPrincipal UserAccountUserDetails userAccountUserDetails,
+            int year, int month, int day, String reservationTime, String memo
+    ) {
+        ReservationRequest reservationRequest = ReservationRequest.of(LocalDate.of(year, month, day), LocalTimeConverter.from(reservationTime), memo, ReservationStatus.READY);
+
+        reservationService.updateReservation(reservationId, reservationRequest.toDto(userAccountUserDetails.toDto()));
+
+        return "redirect:/reservations/my-page";
+    }
+
+    @PostMapping("{reservationId}/delete")
+    public String deleteReservation(
+            @PathVariable("reservationId") Long reservationId,
+            @AuthenticationPrincipal UserAccountUserDetails userAccountUserDetails
+    ) {
+        reservationService.deleteReservation(reservationId, userAccountUserDetails.toDto());
+        return "redirect:/reservations/my-page";
+    }
 }
--- src/main/java/com/honey/reservation/service/ReservationService.java
+++ src/main/java/com/honey/reservation/service/ReservationService.java
@@ -4,7 +4,9 @@ import com.honey.reservation.domain.UserAccount;
 import com.honey.reservation.domain.reservation.Reservation;
 import com.honey.reservation.domain.reservation.ReservationStatus;
 import com.honey.reservation.dto.ReservationDto;
+import com.honey.reservation.dto.UserAccountDto;
 import com.honey.reservation.dto.request.ReservationRequest;
+import com.honey.reservation.dto.security.UserAccountUserDetails;
 import com.honey.reservation.repository.ReservationRepository;
 import com.honey.reservation.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +18,14 @@ import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
 
 @Transactional
 @RequiredArgsConstructor
@@ -40,8 +45,8 @@ public class ReservationService {
                 .limit((END_TIME.toSecondOfDay() - START_TIME.toSecondOfDay()) / (INTERVAL_MINUTES * 60) + 1)
                 .collect(Collectors.toMap(time -> time, time -> true));
 
-        reservationRepository.findByLocalDate(localDate).stream()
-                .map(Reservation::getLocalTime)
+        reservationRepository.findByReservationDate(localDate).stream()
+                .map(Reservation::getReservationTime)
                 .forEach(time -> times.replace(time, false));
 
         return times;
@@ -51,8 +56,19 @@ public class ReservationService {
         validateDuplicateReservation(dto);
         UserAccount userAccount = userAccountRepository.findById(dto.userId()).orElseThrow(() -> new EntityNotFoundException("없는 계정입니다."));
         validateReservationCount(userAccount);
-        validateReservationDateTime(dto.localDate(), dto.localTime());
+        validateReservationDateTime(dto.reservationDate(), dto.reservationTime());
         reservationRepository.save(dto.toEntity(userAccount));
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReservationDto> findMyReservations(UserAccountUserDetails userDetails) {
+        UserAccount userAccount = userAccountRepository.findById(userDetails.getUsername()).orElseThrow(() -> new EntityNotFoundException("없는 계정입니다."));
+        return userAccount.getReservations().stream()
+                .sorted(comparing(Reservation::getReservationDate)
+                        .thenComparing(Reservation::getReservationTime)
+                        .reversed())
+                .map(ReservationDto::from)
+                .collect(Collectors.toList());
     }
 
     private void validateReservationDateTime(LocalDate reservationDate, LocalTime reservationTime) {
@@ -73,10 +89,31 @@ public class ReservationService {
     }
 
     private void validateDuplicateReservation(ReservationDto dto) {
-        if (reservationRepository.findByLocalDateAndLocalTime(dto.localDate(), dto.localTime()).isPresent()) {
+        if (reservationRepository.findByReservationDateAndReservationTime(dto.reservationDate(), dto.reservationTime()).isPresent()) {
             throw new IllegalStateException("이미 예약이 있습니다.");
         }
     }
 
 
+    public void updateReservation(Long reservationId, ReservationDto dto) {
+        Reservation reservation = reservationRepository.getReferenceById(reservationId);
+        UserAccount userAccount = userAccountRepository.getReferenceById(dto.userId());
+        if (reservation.getUserAccount().equals(userAccount)) {
+            if (dto.reservationDate() != null && dto.reservationTime() != null) {
+                reservation.setReservationDate(dto.reservationDate());
+                reservation.setReservationTime(dto.reservationTime());
+            }
+            reservation.setMemo(dto.memo());
+            reservation.setReservationStatus(dto.reservationStatus());
+            reservationRepository.flush();
+        }
+    }
+
+    public void deleteReservation(Long reservationId, UserAccountDto toDto) {
+        Reservation reservation = reservationRepository.getReferenceById(reservationId);
+        UserAccount userAccount = userAccountRepository.getReferenceById(toDto.loginId());
+        if (reservation.getUserAccount().equals(userAccount)) {
+            reservationRepository.delete(reservation);
+        }
+    }
 }
