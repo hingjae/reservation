705553042a6078ad5a26f25799af2e0689381fc0
hingjae <ljh23270@naver.com>
2023-07-11 12:33:30 +0900

    에러 상태코드

--- src/main/java/com/honey/reservation/controller/GlobalCatcher.java
+++ src/main/java/com/honey/reservation/controller/GlobalCatcher.java
@@ -1,26 +1,31 @@
 package com.honey.reservation.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalCatcher {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
     public String exception(Exception ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error/errorPage";
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(EntityNotFoundException.class)
     public String entityNotFoundException(Exception ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error/errorPage";
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalStateException.class)
     public String illegalStateException(Exception ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
